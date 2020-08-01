1a25306e96f2b46dd55fa8f4e0145726699ab6aa
朱飞 <1147786105@qq.com>
2020-08-01 09:33:51 +0800

    Create App.vue

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <calendar></calendar>
+  </div>
+</template>
+
+<script>
+import calendar from "./components/calendar";
+export default {
+  name: 'App',
+  components: {
+    calendar
+  }
+}
+</script>
+
+<style>
+
+</style>
